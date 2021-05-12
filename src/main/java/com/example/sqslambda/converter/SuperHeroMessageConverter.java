72ed05b0fd0e1373226dfb212c27c5ea5a7ef46a
viswanath <viswanath@orite.io>
2021-05-12 09:58:34 +0200

    Resilient message converter

--- src/main/java/com/example/sqslambda/converter/SuperHeroMessageConverter.java
+++ src/main/java/com/example/sqslambda/converter/SuperHeroMessageConverter.java
@@ -4,6 +4,8 @@ import com.example.sqslambda.entity.SuperHero;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jayway.jsonpath.JsonPath;
+import com.jayway.jsonpath.PathNotFoundException;
+import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.converter.AbstractMessageConverter;
@@ -30,9 +32,18 @@ public class SuperHeroMessageConverter extends AbstractMessageConverter {
         message.getHeaders().forEach( (header, value) -> log.debug("Header name: {}, Header value {}", header, value) );
         return Optional.of(message.getPayload())
                 .map( payload -> payload instanceof String ? (String) payload : new String((byte[]) payload))
-                .map( payloadText -> JsonPath.parse(payloadText).read("$['Records'][0]['body']", String.class))
+                .flatMap(this::extractBody)
                 .flatMap(json -> extractEntity(json, SuperHero.class))
                 .orElseThrow( () -> new RuntimeException("Failed to extract an object of type SuperHero from message!") );
+    }
+
+    private Optional<String> extractBody(@NonNull final String sqsJSONPayload) {
+        try {
+            return Optional.ofNullable(JsonPath.parse(sqsJSONPayload).read("$['Records'][0]['body']", String.class));
+        } catch (PathNotFoundException pathNotFoundException) {
+            log.warn("Failed to extract the value for 'body' property from SQS event JSON {}", sqsJSONPayload);
+            return Optional.of(sqsJSONPayload);
+        }
     }
 
     private <T> Optional<T> extractEntity(String messagePayload, final Class<T> clazz) {
